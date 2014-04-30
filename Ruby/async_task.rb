837332f849b5356fe0a7cfadc87e34824bc0159c
tumbling-dice <inujini.dev@gmail.com>
2014-04-30 10:57:06 +0900

    無慈悲な4タブを2タブに
    インデントが…。

--- Ruby/async_task.rb
+++ Ruby/async_task.rb
@@ -1,55 +1,55 @@
 require "thread"
 
 class AsyncTask
-	
-	def initialize(on_background, *params)
-		this = self
-		
-		@task = lambda {
-			Thread.new {
-				begin
-					@result = on_background.call(*params)
-				rescue Exception => ex
-					@exception = ex
-				ensure
-					@is_end = true
-					@on_exuecuted.call(this) if @on_exuecuted
-					kill
-				end
-			}
-		}
-	end
-	
-	def start
-		@thread = @task.call unless @thread
+  
+  def initialize(on_background, *params)
+    this = self
+    
+    @task = lambda {
+      Thread.new {
+        begin
+          @result = on_background.call(*params)
+        rescue Exception => ex
+          @exception = ex
+        ensure
+          @is_end = true
+          @on_exuecuted.call(this) if @on_exuecuted
+          kill
+        end
+      }
+    }
+  end
+  
+  def start
+    @thread = @task.call unless @thread
     return self
-	end
-	
-	def wait
-		@thread.join if @thread
-	end
-	
-	def result
-		raise exception if @exception
-		return @result
-	end
-	
-	def status
-		@thread.status if @thread
-	end
-	
-	def kill
-		Thread.kill(@thread) if @thread
-	end
-	
-	def continue(&on_exuecuted)
-		
-		if @is_end then
-			on_exuecuted.call(self)
-		end
-		
-		@on_exuecuted = on_exuecuted
-		return self
-	end
-	
+  end
+  
+  def wait
+    @thread.join if @thread
+  end
+  
+  def result
+    raise exception if @exception
+    return @result
+  end
+  
+  def status
+    @thread.status if @thread
+  end
+  
+  def kill
+    Thread.kill(@thread) if @thread
+  end
+  
+  def continue(&on_exuecuted)
+    
+    if @is_end then
+      on_exuecuted.call(self)
+    end
+    
+    @on_exuecuted = on_exuecuted
+    return self
+  end
+  
 end
