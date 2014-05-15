3ec5dffa76e2614be29734031853cd8f4b326397
tumbling-dice <inujini.dev@gmail.com>
2014-05-15 17:22:23 +0900

    add comments

--- Java/Android/DynamicFragmentPagerAdapterTest/src/com/example/adapter/DynamicFragmentPagerAdapter.java
+++ Java/Android/DynamicFragmentPagerAdapterTest/src/com/example/adapter/DynamicFragmentPagerAdapter.java
@@ -12,20 +12,30 @@ import android.support.v4.view.PagerAdapter;
 import android.view.View;
 import android.view.ViewGroup;
 
+/**
+ * Fragmentを表示するPagerAdapter（動的変更版）
+ */
 public class DynamicFragmentPagerAdapter extends PagerAdapter {
 	
 	private Fragment _primaryItem;
 	private List<FragmentInfo> _fragments = new ArrayList<FragmentInfo>();
 	private FragmentManager _fm;
-	private FragmentTransaction _ft;
 	private FragmentTransactionProxy _ftp;
 	private boolean _isNeedAllChange;
-
+	
+	/**
+	 * Fragmentの情報を所持するクラス
+	 */
 	protected class FragmentInfo {
 		private Fragment fragment;
 		private CharSequence name;
 		private boolean isShown;
 
+		/**
+		 * Fragmentの情報を所持するクラス
+		 * @param name 表示名
+		 * @param fragment 表示するFragment
+		 */
 		public FragmentInfo(CharSequence name, Fragment fragment) {
 			this.name = name;
 			this.fragment = fragment;
@@ -49,11 +59,27 @@ public class DynamicFragmentPagerAdapter extends PagerAdapter {
 		public void setShown(boolean isShown) {
 			this.isShown = isShown;
 		}
+		
+		@Override
+		public String toString() {
+			return this.name;
+		}
 	}
 	
+	/**
+	 * FragmentTransactionのproxy…と言うか、機能制限版。
+	 * （containerのリソースIDやtagを用いるメソッド、commit関連のメソッドを呼べない）
+	 * DynamicFragmentPagerAdapter（or サブクラス）ではこのproxyを介してFragmentTransactionを制御する。
+	 * add及びcommitは#getTransaction()で何とかする。でも#getTransaction()はサブクラスでは呼ばせたくない。
+	 */
 	protected final class FragmentTransactionProxy {
 		private FragmentTransaction _ft;
 		
+		/**
+		 * FragmentTransactionの機能制限版Proxy
+		 * DynamicFragmentPagerAdapter（or サブクラス）ではこのproxyを介してFragmentTransactionを制御する。
+		 * @param ft FragmentTransactionの実体
+		 */
 		public FragmentTransactionProxy(FragmentTransaction ft) {
 			_ft = ft;
 		}
@@ -87,15 +113,27 @@ public class DynamicFragmentPagerAdapter extends PagerAdapter {
 			return this;
 		}
 		
+		//TODO: inner classのprivate methodってouter classから呼べるんだっけ…？呼べないよね…？
+		//      もし呼べるのならprivateの方がいい package scopeはかなり苦肉の策
 		FragmentTransaction getTransaction() {
 			return _ft;
 		}
+		
 	}
-
+	
+	/**
+	 * Fragmentを表示するPagerAdapter（動的変更版）
+	 * @param fm FragmentManager
+	 */
 	public DynamicFragmentPagerAdapter(FragmentManager fm) {
 		_fm = fm;
 	}
-
+	
+	/**
+	 * Fragmentを表示するPagerAdapter（動的変更版）
+	 * @param fm FragmentManager
+	 * @param fragments 表示名とFragmentのMap（これを用いて表示を初期化する）
+	 */
 	public DynamicFragmentPagerAdapter(FragmentManager fm, Map<CharSequence, Fragment> fragments) {
 		_fm = fm;
 
@@ -119,49 +157,68 @@ public class DynamicFragmentPagerAdapter extends PagerAdapter {
 
 	@Override
 	public final Object instantiateItem(ViewGroup container, int position) {
+		/*
+		 * このメソッドは以下の3パターンで呼ばれる
+		 * 
+		 * 1.今まで表示されていなかったFragmentを新規に表示する必要が出てきた
+		 * 2.非表示になっていたけど再度表示する必要が出てきた
+		 * 3.#getItemPosition(Object)でPOSITION_NONEが返された
+		 * 
+		 * 注意する必要があるのは、1のケースだとFragmentManagerに未登録である、と言うこと。
+		 * 裏を返せばそれ以外のケースでは（明示的にremoveしていない限り）必ずFragmentManagerに登録されている。
+		 * 
+		 * #getItemPosition(Object)が呼ばれるのはPagerAdapter#notifyDataSetChanged()が呼ばれたときだけである。（多分…）
+		 */
 		
 		FragmentInfo fi = _fragments.get(position);
+		Fragment registFragment = fi.getFragment();
+		
 		StringBuilder tag = new StringBuilder();
 		tag.append(container.getId()).append(":").append(fi.getName());
-
-		// destroyItemでdetachされていたFragmentの場合はattachする
-		Fragment f = _fm.findFragmentByTag(tag.toString());
 		
+		
+		// ここに来る前にFragmentTransaction#remove(Fragment)を呼んでいないと、
+		// どうあがいてもFragmentManagerのキャッシュが表示されてしまうので注意。
+		Fragment f = _fm.findFragmentByTag(tag.toString());
 		transaction();
 		
-		if(f != null && fi.getFragment().equals(f)) {
-			_ft.attach(f);
+		// FragmentInfoに登録されているFragmentと一致するかどうかをチェックし、
+		// 一致する（＝変更がない）場合はそのままattachする。
+		if(registFragment.equals(f)) {
+			_ftp.attach(f);
 			return f;
 		}
-
-		f = fi.getFragment();
-
-		if(!f.equals(_primaryItem)) {
-			f.setMenuVisibility(false);
-			f.setUserVisibleHint(false);
+		
+		// 一致しない場合はremoveメソッドなどによって位置が変わっているので、
+		// FragmentInfoに登録されているfragmentを返す。
+		if(!registFragment.equals(_primaryItem)) {
+			registFragment.setMenuVisibility(false);
+			registFragment.setUserVisibleHint(false);
 		}
 
 		fi.setShown(true);
-		_ft.add(container.getId(), f, tag.toString());
+		_ftp.getTransaction().add(container.getId(), registFragment, tag.toString());
 
-		return f;
+		return registFragment;
 	}
 
 	@Override
 	public final void destroyItem(ViewGroup container, int position, Object object) {
-		// 本来はViewPager（container）の中からView（object）を取り除く処理
-		// PagerAdapter#getItemPositionでPOSITION_NONEが返ってくるとこれが呼ばれる
-		// と言うか、思っている以上にViewPagerの色んなところから呼ばれる
-
-		// 厄介なのは「画面上で表示しきれなくなったobjectもここを通過する」と言う点だろう
-		// その一点のためだけにFragmentPagerAdapterとFragmentStatePagerAdapterの処理が意味不明になっていると言っても過言ではない
-
-		// このクラスではあくまでもFragmentTransaction#detach(Fragment)しか行わない
-		// 本当に削除する必要がある場合は各メソッドでFragmentTransaction#remove(Fragment)を呼び出す必要がある
+		/*
+		 * 本来はViewPager（container）の中からView（object）を取り除く処理。
+		 * PagerAdapter#getItemPositionでPOSITION_NONEが返ってくるとこれが呼ばれる。
+		 * と言うか、思っている以上にViewPagerの色んなところから呼ばれる。
+		 * 
+		 * 厄介なのは「画面上で表示しきれなくなったobjectもここを通過する」と言う点だろう。
+		 * その一点のためだけにFragmentPagerAdapterとFragmentStatePagerAdapterの処理が意味不明になっていると言っても過言ではない。
+		 * 
+		 * このメソッドではあくまでもFragmentTransaction#detach(Fragment)しか行わない。
+		 * 本当に削除する必要がある場合は各メソッドでFragmentTransaction#remove(Fragment)を呼び出す必要がある。
+		 */
 		
 		Fragment fragment = (Fragment)object;
 		transaction();
-		_ft.detach(fragment);
+		_ftp.detach(fragment);
 	}
 
 	@Override
@@ -185,9 +242,20 @@ public class DynamicFragmentPagerAdapter extends PagerAdapter {
 
 	@Override
 	public final void finishUpdate(ViewGroup container) {
-		if(_ft != null) {
-			_ft.commitAllowingStateLoss();
-			_ft = null;
+		/*
+		 * FragmentTransactionのcommitはここで行う。
+		 * notifyDataSetChangedを呼ばなくてもタブの移動etcでFragmentが表示しきれなくなると普通に呼ばれる。
+		 * その場合でもFragmentTransaction#detachのcommitはしなくてはならないので、
+		 * notifyDataSetChangedを呼ばなくても勝手に更新されてしまう。
+		 *
+		 * そもそもPagerAdapterとFragmentManagerの相性が悪いとしか言いようがない。
+		 * ViewPagerのメソッドをほとんどfinalにしたのもその一言で大体片付く。
+		 * って言うか、destroyItemとfinishUpdateに色々な役割を持たせすぎだと思う…。
+		 */
+		
+		if(_ftp != null) {
+			_ftp.getTransaction().commitAllowingStateLoss();
+			_ftp = null;
 			_fm.executePendingTransactions();
 		}
 
@@ -199,6 +267,8 @@ public class DynamicFragmentPagerAdapter extends PagerAdapter {
 		return ((Fragment)object).getView() == view;
 	}
 
+	//TODO: この辺必要なのかな
+	//      stateの保存は各々のFragmentで管理してもらえると非常にありがたいんだが
 	/*@Override
 	public Parcelable saveState() {
 		Bundle state = new Bundle();
@@ -236,11 +306,26 @@ public class DynamicFragmentPagerAdapter extends PagerAdapter {
 
 	@Override
 	public final int getItemPosition(Object object) {
+		/*
+		 * PagerAdapter#notifyDataSetChangedが呼ばれるとここを通過する。
+		 * と言ってもViewPagerが持っているItem（object）が全部来るわけではなく、
+		 * 現在表示されているItemだけがやってくる。
+		 * 
+		 * remove、insert、replaceが呼ばれた時は表示されているFragmentに影響が出る可能性があるため、
+		 * 必ずPOSITION_NONEを返すようにする。
+		 * 
+		 * ここでPOSITION_NONEを返すと次はdestoryItemが呼ばれ、その次にinstantiateItemが呼ばれる。
+		 * 表示されているFragmentに影響がなければ、一旦detachして直後にattachするだけである。
+		 * 
+		 * ちなみにattach / detachは既にremoveされているFragmentに実行しても何も起こらない。例外も出ない。
+		 * そのおかげで「notifyDataSetChanged前にFragmentTransaction#remove(Fragment)を呼び出す」と言う
+		 * 一見危なっかしい行為がまかり通るようになっている。
+		 */
 		return _isNeedAllChange ? POSITION_NONE : POSITION_UNCHANGED;
 	}
 	
 	/**
-	 * Fragmentを追加します
+	 * Fragmentを追加します。
 	 * @param name タブの表示名
 	 * @param fragment 追加するFragment
 	 */
@@ -251,7 +336,7 @@ public class DynamicFragmentPagerAdapter extends PagerAdapter {
 	}
 
 	/**
-	 * Fragmentを取得します
+	 * Fragmentを取得します。
 	 * @param position 0から始まる取得する場所
 	 * @return positionで指定された位置のFragment
 	 */
@@ -260,18 +345,18 @@ public class DynamicFragmentPagerAdapter extends PagerAdapter {
 	}
 
 	/**
-	 * Fragmentを削除します
+	 * Fragmentを削除します。
 	 * @param position 0から始まる削除する場所
 	 */
 	public void remove(int position) {
 		transaction();
-		_ft.remove(_fragments.getFragment());
+		_ftp.remove(_fragments.getFragment());
 		_fragments.remove(position);
 		_isNeedAllChange = true;
 	}
 
 	/**
-	 * Fragmentを入れ替えます
+	 * Fragmentを入れ替えます。
 	 * @param position 0から始まる入れ替える位置
 	 * @param name 新しい表示名
 	 * @param fragment 入れ替え後に表示するFragment
@@ -280,10 +365,16 @@ public class DynamicFragmentPagerAdapter extends PagerAdapter {
 		if(hasName(name, position)) throw new IllegalArgumentException("表示名が重複しています。");
 
 		FragmentInfo fi = _fragments.get(position);
-
+		
+		// 一度でも表示されてしまったFragmentをreplaceする場合は
+		// 事前にFragmentTransactionProxy#remove(Fragment)を呼び出して削除しておく。
+		// （これをしておかないとinstantiateItemでFragmentManagerのキャッシュを返してしまう。）
+		
+		// また、一度も表示されていない = 現在も表示されていない、となるので、
+		// _isNeedAllChangeは一度でも表示されていた場合にだけtrueにする。
 		if(fi.isShown()) {
 			transaction();
-			_ft.remove(fi.getFragment());
+			_ftp.remove(fi.getFragment());
 			_isNeedAllChange = true;
 		}
 
@@ -291,7 +382,7 @@ public class DynamicFragmentPagerAdapter extends PagerAdapter {
 	}
 
 	/**
-	 * Fragmentを挿入します
+	 * Fragmentを挿入します。
 	 * @param position 0から始まる挿入する位置（元々あったFragmentは右にずれる）
 	 * @param name 表示名
 	 * @param fragment 挿入するFragment
@@ -303,57 +394,49 @@ public class DynamicFragmentPagerAdapter extends PagerAdapter {
 	}
 	
 	/**
-	 * Adapterに設定されているFragmentManagerを取得します
-	 * @return FragmentManager
-	 */
-	protected FragmentManager getFragmentManager() {
-		return _fm;
-	}
-	
-	/**
-	 * FragmentTransactionのProxyを取得します
+	 * FragmentTransactionのProxyを取得します。
 	 * @return FragmentTransactionProxy
 	 */
-	protected FragmentTransactionProxy getFragmentTransactionProxy() {
+	protected final FragmentTransactionProxy getFragmentTransactionProxy() {
 		transaction();
-		return new FragmentTransactionProxy(_ft);
+		return _ftp;
 	}
 	
 	/**
-	 * PagerAdapter#notifyDataSetChanged時にFragmentを再配置するフラグを設定します
-	 * 既存のFragmentの位置が変更される場合は必ずtrueにする必要があります
+	 * PagerAdapter#notifyDataSetChanged時にFragmentを再配置するフラグを設定します。
+	 * 既存のFragmentの位置が変更される場合は必ずtrueにする必要があります。
 	 * また、notifyDataSetChanged後、もしくはnotifyDataSetChanged前にタブを移動するなどの操作が行われた場合、
-	 * このフラグは強制的にfalseになります
+	 * このフラグは強制的にfalseになります。
 	 * @see PagerAdapter#finishUpdate()
 	 * @see PagerAdapter#notifyDataSetChanged()
 	 */
-	protected void needAllChange() {
+	protected final void needAllChange() {
 		_isNeedAllChange = true;
 	}
 	
 	/**
-	 * FragmentInfoを取得します
+	 * FragmentInfoを取得します。
 	 * @param position 0から始まる取得する位置
 	 * @return positionで指定された位置のFragmentInfo
 	 */
-	protected FragmentInfo getFragmentInfo(int positon) {
+	protected final FragmentInfo getFragmentInfo(int positon) {
 		return _fragments.get(position);
 	}
 	
 	/**
-	 * FragmentInfoのリストを取得します
+	 * FragmentInfoのリストを取得します。
 	 * @return FragmentInfoのリスト
 	 */
-	protected List<FragmentInfo> getFragmentInfoes() {
+	protected final List<FragmentInfo> getFragmentInfoes() {
 		return _fragments;
 	}
 	
 	/**
-	 * 表示名の重複があるかどうかを調査します
+	 * 表示名の重複があるかどうかを調査します。
 	 * @param name 調査する表示名
 	 * @return 重複する表示名があればtrue
 	 */
-	protected boolean hasName(CharSequence name) {
+	protected final boolean hasName(CharSequence name) {
 		for (FragmentInfo fi : _fragments) {
 			if(fi.getName().equals(name)) return true;
 		}
@@ -361,12 +444,12 @@ public class DynamicFragmentPagerAdapter extends PagerAdapter {
 	}
 
 	/**
-	 * 表示名の重複があるかどうかを調査します
+	 * 表示名の重複があるかどうかを調査します。
 	 * @param name 調査する表示名
 	 * @param position 調査を除外する位置
 	 * @return 指定されたpositonと違う位置に重複する表示名がある場合はtrue
 	 */
-	protected boolean hasName(CharSequence name, int position) {
+	protected final boolean hasName(CharSequence name, int position) {
 		for (int i = 0, size = _fragments.size(); i < size; i++) {
 			if(_fragments.get(i).getName().equals(name)){
 				return i != position ? true : false;
@@ -376,14 +459,7 @@ public class DynamicFragmentPagerAdapter extends PagerAdapter {
 	}
 	
 	private void transaction() {
-		
-		if(_ftp != null) {
-			_ft = _ftp.getTransaction();
-			_ftp = null;
-			return;
-		}
-		
-		if(_ft == null) _ft = _fm.beginTransaction();
+		if(_ftp == null) _ftp = new FragmentTransactionProxy(_fm.beginTransaction());
 	}
 
 }
